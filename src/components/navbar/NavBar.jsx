d0f7f2101a1c1e7fc15987dc46ecdfae849c4674
Enrique Rocha <sistema@powerupsbusiness.com>
2023-08-22 17:09:49 -0600

    8-22-2023

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,9 +1,13 @@
+"use client";
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from '../../styles/navbar/navbar.module.css';
 import logo from '../../images/cryptomex.png';
-import Link from 'next/link';
+import visaIcon from '../../../public/visa.svg';
+import mastercardIcon from '../../../public/mastercard.svg';
 
 export const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className={styles.main}>
@@ -11,24 +15,34 @@ export const Navbar = () => {
         <a
           href="/"
           rel="noopener noreferrer"
-          className={ styles["logo-a"] }
+          className={styles["logo-a"]}
         >
           <Image
-            src={ logo }
+            src={logo}
             alt="Cryptospace Logo"
-            className= {styles.logo}
+            className={styles.logo}
             priority
           />
         </a>
         <div className={styles.menu}>
-          <a href="/prestabit">
-              Prestabit
+          <a href="https://cryptospace.global/" 
+             className={styles.visamaster}
+             target="_blank" 
+             rel="noopener noreferrer"
+          >
+              Compra Cryptos con 
+              <Image src={visaIcon} alt="Visa Icon" className={styles.cardIcon} />
+              <Image src={mastercardIcon} alt="Mastercard Icon" className={styles.cardIcon} />
           </a>
-          <a href="/sistema-qr">
-              Sistema QR
-          </a>
+          <button className={styles.hamburger} onClick={() => setMenuOpen(!isMenuOpen)}>
+            ≡
+          </button>
+        </div>
+        <div className={`${styles.dropdown} ${isMenuOpen ? styles.active : ""}`}>
+            <a href="/prestabit">Prestabit</a>
+            <a href="/sistema-qr">Sistema QR</a>
         </div>
       </div>
     </div>
-  )
+  );
 }
